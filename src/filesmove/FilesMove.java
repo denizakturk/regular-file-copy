0c31ae5ae56429b953fd4234dc4dc9bcb8f04f92
Deniz <deniz.akturk@sanssende.com>
2015-07-15 12:09:47 +0300

    Begin files

---
+++ src/filesmove/FilesMove.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filesmove;
+
+import java.io.File;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author yonetici
+ */
+public class FilesMove {
+
+    public static JFrame filesFrame = new FilesFrame();
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        filesFrame.setVisible(true);
+    }
+    
+}
